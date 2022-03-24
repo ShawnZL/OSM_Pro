3898dd2050a52e57872043a20f61fb6c0341fd3a
Shawn <1140068696@qq.com>
2022-03-24 23:19:29 +0800

    20220324

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import json
 import xml.dom.minidom
 import folium
 import pandas as pd
+
+pot_dic = [(113.2313871208018, 23.140162974094363), (111.93879502979422, 16.425209561404067),
+           (112.84744887594812, 9.45900895390399), (119.55059291659451, 24.4931170714513),
+           (119.6039949335827, 23.34597666972716), (116.26636887181913, 21.17155493782393),
+           (116.47997693977199, 20.847518200614818), (110.57905406257396, 16.57475898343618),
+           (111.08637322396203, 16.11356470493772), (113.70307205638468, 15.933919557307531),
+           (113.91668012433756, 15.599867069991507), (118.32234652586548, 15.31678143014812),
+           (117.36111022007758, 15.162208993475758), (111.59369238535008, 9.767571538365777),
+           (112.15441356372637, 9.425316332883368), (103.87710093055264, 4.630185811747029),
+            (100.32586680083617, 11.970076546572242)]
 def get_way():
     dom = xml.dom.minidom.parse('sansha.osm')
     root = dom.documentElement
@@ -56,6 +66,7 @@ def makr_pic1(node_list):
     ).add_to(sea_map)
 def make_pic():
     sea_map = folium.Map(location=[17.1434, 113.115], zoom_start=4)
+
     for i in range (0, 12):
         name = 'temp' + str(i) + '.json'
         f = open(name,)
@@ -71,6 +82,16 @@ def make_pic():
             weight=0.5, #线的大小为3
             opacity=0.8 #线的透明度
         ).add_to(sea_map)
+
+    for i in range(0, 17):
+        print(i)
+        folium.Marker(
+            location = [pot_dic[i][1], pot_dic[i][0]],
+            popup=i,
+            icon = folium.Icon(color='green', icon='100px')
+        ).add_to(sea_map)
+        print(i)
+
     sea_map.save('map.html')
 
 def print_hi(name):
@@ -82,4 +103,5 @@ def print_hi(name):
 if __name__ == '__main__':
     #get_way()
     make_pic()
+
 
